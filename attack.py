f27477516e018cdb8c0a08819ff162f4a7e61725
Keith McCammon <keith@mccammon.org>
2018-10-26 10:58:23 -0600

    Add reference output of --dump-data-sources

--- attack.py
+++ attack.py
@@ -35,15 +35,33 @@ def data_source_match(data_sources, match_list):
   return match
 
 
+def parse_data_source_list(path):
+  ret = set()
+
+  with open(path, 'r') as input_file:
+    for data_source in input_file.readlines():
+      ret.add(data_source.strip().lower())
+
+  return list(ret)
+
+
 def main():
   parser = argparse.ArgumentParser()
   parser.add_argument("--matrix", type=str, action="store", 
                       default="enterprise",
                       help="Matrix to query (enterprise, mobile, pre)")
+
+  # Tools related to Data Sources
   parser.add_argument("--dump-data-sources", action="store_true",
                       help="Dump data sources to data_sources.txt")
+  parser.add_argument("--match-data-sources", type=str, action="store",
+                      help="A file containing a list of data sources that to match against techniques.")
 
   args = parser.parse_args()
+
+  match_data_sources = None
+  if args.match_data_sources:
+    match_data_sources = parse_data_source_list(args.match_data_sources)
  
   args.matrix = args.matrix.lower()
   if args.matrix == 'pre':
@@ -74,29 +92,30 @@ def main():
 
   technique_count = 0
   techniques_without_data_source = 0
-  techniques_cb_observable = 0
+  techniques_observable = 0
   data_sources = set()
 
   for technique in all_techniques:
     technique_count += 1
-   
+  
     if 'x_mitre_data_sources' in technique.keys():
-      if data_source_match(technique['x_mitre_data_sources'],
-                           #TODO: Make the match list a passable argument.
-                           match_list=crowdstrike_data_sources) == True:
-        techniques_cb_observable += 1
-        
+      if match_data_sources is not None:
+        if data_source_match(technique['x_mitre_data_sources'],
+                            match_list=match_data_sources) == True:
+          techniques_observable += 1
+
       if args.dump_data_sources == True:
         [data_sources.add(data_source) for data_source in technique['x_mitre_data_sources']]
 
     else:
       techniques_without_data_source += 1
 
-  print('Techniques: {0}'.format(technique_count))
-  print('Techniques Observable: {0} ({1}%)'\
-    .format(techniques_cb_observable,
-            round((techniques_cb_observable / technique_count) * 100)))
-
+  if match_data_sources is not None:
+    print('Techniques: {0}'.format(technique_count))
+    print('Techniques Observable: {0} ({1}%)'\
+      .format(techniques_observable,
+              round((techniques_observable / technique_count) * 100)))
+        
   if args.dump_data_sources == True:
     with open('data_sources.txt', 'w') as fh_data_sources:
       data_sources = list(data_sources)
